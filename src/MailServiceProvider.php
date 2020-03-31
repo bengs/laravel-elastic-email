84d18b0b68739f1d0397c50d515261661375a67a
Clay Bitner <claybitner@gmail.com>
2020-03-31 17:50:11 -0600

    Adding MailManger to MailServiceProvider for Laravel 7

--- src/MailServiceProvider.php
+++ src/MailServiceProvider.php
@@ -17,4 +17,20 @@ class MailServiceProvider extends LaravelMailServiceProvider
 		return new TransportManager($app);
 	});
     }
+
+    /**
+     * Register the Illuminate mailer instance.
+     * For Laravel 7
+     * @return void
+     */
+    protected function registerIlluminateMailer()
+    {
+        $this->app->singleton('mail.manager', function ($app) {
+            return new MailManager($app);
+        });
+
+        $this->app->bind('mailer', function ($app) {
+            return $app->make('mail.manager')->mailer();
+        });
+    }
 }
